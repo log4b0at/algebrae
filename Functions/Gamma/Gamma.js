e9db9b4376685ec26dc942277d61ec55dbcc451b
Léo Sarrazin <alpiniste96@gmail.com>
2020-10-23 00:58:28 +0200

    Add Gamma and SinPi functions + Gamma tests

---
+++ Functions/Gamma/Gamma.js
@@ -0,0 +1,79 @@
+const { sinpi } = require('../SinPi/SinPi')
+
+function binet(x)
+{
+	const ix = 1 / x;
+	const ix2 = ix * ix;
+	const ix4 = ix2 * ix2;
+	const ix6 = ix4 * ix2;
+	const ix8 = ix4 * ix4;
+	const ix10 = ix4 * ix6;
+	const ix12 = ix6 * ix6;
+	return (
+		(1. / 12.) - ix2 / 360. + ix4 / 1260.
+		- ix6 / 1680. + ix8 / 1188. - ix10 * (691/360360) 
+		+ ix12 / 156.
+	) * ix;
+}
+
+function pole(x)
+{
+	return 1 / (((((((((((((((((((
+		(7.782263439905071e-12*x + 1.0434267116911005e-10) * x + -1.18127457048702e-9) * x + 5.002007644469223e-9) * x 
+		+ 6.116095104481416e-9) * x + -2.056338416977607e-7) * x + 0.000001133027231981696) * x + -0.0000012504934821426706) * x 
+		+ -0.00002013485478078824) * x + 0.0001280502823881162) * x + -0.00021524167411495098) * x + -0.0011651675918590652) * x 
+		+ 0.0072189432466631) * x + -0.009621971527876973) * x + -0.04219773455554433) * x + 0.16653861138229148) * x 
+		+ -0.04200263503409524) * x + -0.6558780715202539) * x + 0.5772156649015329) * x + 1) * x)
+}
+
+function positive(x) {
+	let o = x;
+	if (x < 10)
+		x += 10;
+	let b = binet(x), offset = x, power = x + .5;
+	if (o > 142)
+		power /= 2;
+	let result = Math.pow(x, power);
+	let last = result;
+	result = result + result * Math.expm1(b);
+	result /= Math.exp(offset);
+	if (o > 142)
+		result *= last;
+	result = result * 2 + result * 0.506628274631000502415765;
+	if (o < 10)
+		result /= (o + 1) * (o + 2) * (o + 3) * (o + 4) * (o + 5) * (o + 6) * (o + 7) * (o + 8) * (o + 9) * (o + 10);
+	return result;
+}
+
+function reflection(x)
+{
+	const xpi = 4 * x - 0.8584073464102067615 * x;
+	return xpi / (sinpi(x) * positive(-x));
+}
+
+/**
+ * Compute `°𝚪(x)` where 𝚪 is Euler gamma function
+ * @param {number} x A float64 number
+ */
+function gamma(x)
+{
+	if (0 < x && x < 0.5)
+		return pole(x);
+	else
+		return p1gamma(x - 1);
+}
+
+/**
+ * Compute `°𝚪(x + 1)` where 𝚪 is Euler gamma function
+ * @param {number} x A float64 number
+ */
+function p1gamma(x) {
+	if (x === -1)
+		return NaN;
+	else if (x < 0)
+		return reflection(x);
+	else
+		return positive(x);
+}
+
+module.exports = { gamma, p1gamma }
